301a19db4c6ebad424bcf5d47c7dee429593f522
Doug Perkes <dougper@microsoft.com>
2017-02-04 14:25:28 -0800

    Added initial category selection

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var builder = require('botbuilder');
 // Bot Setup
 //=========================================================
 
+var newsCats = ["World", "National", "Sports", "Food"]
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -23,6 +25,18 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.choice(session, "Which category?", newsCats)
+    },
+    function (session, results) {
+        if (results.response) {
+            // var region = salesData[results.response.entity];
+            session.send("You selected " + JSON.stringify(results.response.entity));
+            // session.send("We sold %(units)d units for a total of %(total)s.", region); 
+        } else {
+            session.send("ok");
+        }
+    }
+]
+);
